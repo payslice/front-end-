2464e4c98944dde24e52e89a9a09763ffcc69ce3
nathmankind <nathmankind@gmail.com>
2021-12-13 10:43:07 +0100

    complete user auth

--- src/pages/EmployeePages/Auth/OTPReset.jsx
+++ src/pages/EmployeePages/Auth/OTPReset.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { Button } from "../../../components/Button/Button";
 import { InputField, PasswordInput } from "../../../components/Input";
+import successIcon from "../../../assets/svgs/success.svg";
 
 export const OTPReset = () => {
+  const [resetSuccess, setResetSuccess] = useState(false);
   const history = useHistory();
   const submitForm = (e) => {
     e.preventDefault();
@@ -11,33 +13,50 @@ export const OTPReset = () => {
   };
   return (
     <>
-      <div className="p-10 flex flex-col h-full justify-center w-3/4">
-        <h1 className="text-3xl font-bold  uppercase">enter otp</h1>
+      {!resetSuccess ? (
+        <div className="p-10 flex flex-col h-full justify-center w-3/4">
+          <h1 className="text-3xl font-bold  uppercase">enter otp</h1>
 
-        <form onSubmit={submitForm}>
-          <div className="mt_10">
-            <InputField
-              label="ENter OTP"
-              required
-              type="text"
-              placeholder="e.g Kelly@farfill.com"
-            />
-            <PasswordInput
-              required
-              label="Set password"
-              placeholder="Enter password"
-            />
-            <PasswordInput
-              required
-              label="Confirm password"
-              placeholder="Enter password"
-            />
+          <form onSubmit={submitForm}>
+            <div className="mt_10">
+              <InputField
+                label="Enter OTP"
+                required
+                type="text"
+                placeholder="e.g Kelly@farfill.com"
+              />
+              <PasswordInput
+                required
+                label="Set password"
+                placeholder="Enter password"
+              />
+              <PasswordInput
+                required
+                label="Confirm password"
+                placeholder="Enter password"
+              />
+            </div>
+            <div className="signUp__submit-btn flex justify-end">
+              <Button
+                type="submit"
+                buttonText="Next"
+                onClick={() => setResetSuccess(true)}
+              />
+            </div>
+          </form>
+        </div>
+      ) : (
+        <div className="my-20 text-center">
+          <div className="text-gray-500 uppercase text-2xl">
+            your password has been changed
           </div>
-          <div className="signUp__submit-btn flex justify-end">
-            <Button type="submit" buttonText="Next" />
-          </div>
-        </form>
-      </div>
+          <img src={successIcon} alt="" className="mx-auto my-16" />
+
+          <a href="/user/login" className="text-gray-500 hover:text-gray-500">
+            Click here login
+          </a>
+        </div>
+      )}
     </>
   );
 };
---
+++ src/pages/EmployeePages/Auth/ResetSuccess.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import successIcon from "../../assets/svgs/success.svg";
+
+const ResetSuccess = () => {
+  return (
+    <div className="my-20 text-center">
+      <div className="text-blue-600 text-2xl">
+        Congratulations, Yes you did it
+      </div>
+      <img src={successIcon} alt="" className="mx-auto my-16" />
+
+      <a href="/user/login">Click here login</a>
+    </div>
+  );
+};
+
+export default ResetSuccess;
