fa17f606f9a21ee56cfa320f66d575a27df5c8a0
nathmankind <nathmankind@gmail.com>
2021-12-26 19:23:12 +0100

    connect mono implementation

--- src/pages/CompanyOnboard/LinkWithMono.jsx
+++ src/pages/CompanyOnboard/LinkWithMono.jsx
@@ -1,26 +1,51 @@
 import React, { useState } from "react";
 import { Button } from "../../components/Button/Button";
 import OnboardSuccess from "./OnboardSuccess";
+import { MonoButton, useMono } from "react-mono-js";
 
 const LinkWithMono = () => {
   const [linked, setLinked] = useState(false);
+  const config = {
+    public_key: "test_pk_4ic2CNCGRugJ8OGdsVth",
+    onClose: () => {},
+    onSuccess: (response) => {
+      console.log(response.code);
 
+      /**
+        response : { "code": "code_xyz" }
+        you can send this code back to your server to get this
+        authenticated account and start making requests.
+      */
+    },
+  };
+
+  const handleMono = useMono({
+    public_key: `${process.env.REACT_APP_MONO_PUBLIC_KEY}`,
+  });
   return (
     <div>
       {!linked ? (
         <>
           <div className="text-2xl mb-10">Link account Details with MONO </div>
           <p>
-            Companies who connect atleast two bank account have chances of full
+            Companies who connect at least two bank account have chances of full
             payroll support.
           </p>
-          <p>Naira or Mono doesn't have access to move your funds.</p>
+          <p>Mono doesn't have access to move your funds.</p>
 
           <div className="signUp__submit-btn mt-20 flex justify-start">
             <Button
               type="submit"
               buttonText="Connect Bank"
-              onClick={() => setLinked(true)}
+              onClick={() => {
+                handleMono({
+                  onClose: () => null,
+                  onSuccess: (response) => {
+                    console.log(response.code);
+                    setLinked(true);
+                  },
+                });
+              }}
             />
           </div>
         </>
