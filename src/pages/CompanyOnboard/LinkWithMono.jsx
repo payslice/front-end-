03bff41dabbe61f59dca8b4fb499836871a399ea
nathmankind <nathmankind@gmail.com>
2021-12-26 19:23:48 +0100

    remove unused code on mono link page

--- src/pages/CompanyOnboard/LinkWithMono.jsx
+++ src/pages/CompanyOnboard/LinkWithMono.jsx
@@ -1,23 +1,10 @@
 import React, { useState } from "react";
 import { Button } from "../../components/Button/Button";
 import OnboardSuccess from "./OnboardSuccess";
-import { MonoButton, useMono } from "react-mono-js";
+import { useMono } from "react-mono-js";
 
 const LinkWithMono = () => {
   const [linked, setLinked] = useState(false);
-  const config = {
-    public_key: "test_pk_4ic2CNCGRugJ8OGdsVth",
-    onClose: () => {},
-    onSuccess: (response) => {
-      console.log(response.code);
-
-      /**
-        response : { "code": "code_xyz" }
-        you can send this code back to your server to get this
-        authenticated account and start making requests.
-      */
-    },
-  };
 
   const handleMono = useMono({
     public_key: `${process.env.REACT_APP_MONO_PUBLIC_KEY}`,
