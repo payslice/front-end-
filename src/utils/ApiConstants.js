daff67d27bf45ceb83fb76f4c0454541e7a8860b
nathmankind <nathmankind@gmail.com>
2021-12-28 20:27:55 +0100

    remove s from http on base url

--- src/utils/ApiConstants.js
+++ src/utils/ApiConstants.js
@@ -1,5 +1,5 @@
 export const constant = {
   tokenName: "PAYSL-ADTK",
   // expiryName: "UPL-EXP",
-  baseUrl: "https://18.221.73.198/backend/api",
+  baseUrl: "http://18.221.73.198/backend/api",
 };
