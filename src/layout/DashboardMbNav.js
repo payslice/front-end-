04bf5ee9c0c1992b9cb7e7bea381f365c97a10c2
holabisii <ajoseholabisi@gmail.com>
2022-08-21 19:23:47 -0400

    configured logout for employee

--- src/layout/DashboardMbNav.js
+++ src/layout/DashboardMbNav.js
@@ -8,16 +8,27 @@ import { MdArrowBackIosNew } from 'react-icons/md';
 import { AiFillHome, AiFillSetting } from 'react-icons/ai';
 import { MdAnalytics } from 'react-icons/md';
 import { FaCommentDots } from 'react-icons/fa';
+import { persistSelector } from '../slices/persist';
+import { useSelector } from 'react-redux/es/exports';
+import { MdKeyboardArrowDown } from 'react-icons/md';
+import { useHistory } from 'react-router-dom';
+import { removeTokenFromStorage, removeUserDataFromStorage } from '../utils/ApiUtils'
 
 export const DahboardMobileNav = () => {
 	const tdc = useRef();
 	const [showMenu, setShowMenu] = useState(false);
+	const [show, setShow] = useState(false);
+
+	const { user } = useSelector(persistSelector)
+
 	useClickOutside(tdc, () => {
 		setShowMenu(false);
 	});
 
 	const location = useLocation();
 	const isEmployee = location?.pathname?.includes('user');
+
+	const history = useHistory()
 
 	const menuItems = [
 		{
@@ -77,6 +88,12 @@ export const DahboardMobileNav = () => {
 
 	const activeMenuList = isEmployee ? userMenuList : menuItems;
 
+	const handleLogout = () => {
+		history.push('/user/login')
+		removeTokenFromStorage()
+		removeUserDataFromStorage()
+	}
+
 	return (
 		<div
 			ref={tdc}
@@ -98,11 +115,45 @@ export const DahboardMobileNav = () => {
 					}}
 				/>
 
-				<div>
-					<button className="text-white font-bold py-1 px-3 rounded" style={{background: "#D0000C"}}>
-						0
-					</button>
+				{
+					/*
+					
+						<div>
+							<button className="text-white font-bold py-1 px-3 rounded" style={{background: "#D0000C"}}>
+								0
+							</button>
+						</div>
+					*/
+				}
+				<div
+					onClick={() => setShow(!show)}
+					className="flex items-center bg-gray-100 rounded w-[196px] h-[61px] pl-4 cursor-pointer"
+				>
+					<img
+						src={require('../assets/imgs/user-payslice.jpg')}
+						alt="notification"
+						width="38"
+						height="41"
+						className="object-cover"
+					/>
+					<div className="text-gray-400 my-auto ml-2">
+						<h3 className="text-base font-semibold text-gray-400 mb-0 truncate w-20">{`${user?.first_name} ${user?.last_name}`}</h3>
+						<p className="font-light text-[10px] mb-0 capitalize">{user?.section} account</p>
+					</div>
+					<MdKeyboardArrowDown className="my-auto w-5 h-5 ml-3 text-[#737A91]" />
 				</div>
+
+
+				
+				{show && (
+					<div className="bg-white z-10 w-24 mr-6 border-gray-200 text-left rounded text-xs absolute right-14 top-20 shadow-xl font-semibold">
+						<div className="hover:bg-gray-100  py-3 px-4 border-gray-200 cursor-pointer text-base" onClick={handleLogout}>
+							Logout
+						</div>
+					</div>
+				)}
+
+
 			
 			</div>
 			<div className=''>
