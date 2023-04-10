b3963d79d56f835225fe1dd79bb3ee0d8d77f1de
holabisii <ajoseholabisi@gmail.com>
2023-04-10 15:03:37 -0400

    dashboard and sidebar changes

--- src/layout/Sidebar.js
+++ src/layout/Sidebar.js
@@ -7,7 +7,8 @@ import { useHistory } from 'react-router-dom';
 // import Contact from "../assets/svgs/Contact";
 import SidebarItem from './SidebarItem.js';
 import { AiFillHome, AiFillSetting } from 'react-icons/ai';
-import { FaCommentDots, FaFolder } from 'react-icons/fa';
+import { FaFolder } from 'react-icons/fa';
+// import { FaCommentDots, FaFolder } from 'react-icons/fa';
 import { MdAnalytics } from 'react-icons/md';
 import { HiBriefcase } from 'react-icons/hi';
 import { Link } from 'react-router-dom';
@@ -40,20 +41,19 @@ const Sidebar = () => {
 			<div className="mt-24">
 				<ul className="px-8 mt-10">
 					<SidebarItem currentPath={currentLocation} Icon={AiFillHome} path="/dashboard" caption="Dashboard" isDb />
-					<SidebarItem currentPath={currentLocation} Icon={FaFolder} path="/employee" caption="Employees" isDb />
+					<SidebarItem currentPath={currentLocation} Icon={FaFolder} path="/employee" caption="Wallets" isDb />
 					<SidebarItem
 						currentPath={currentLocation}
 						Icon={MdAnalytics}
 						path="/withdrawals"
-						caption="Withdrawals"
+						caption="Request money"
 						isDb
 					/>
 
 					<div className="border-white border-b-2 w-10 mb-3 mt-3 ml-[26px]"></div>
 					<SidebarItem currentPath={currentLocation} Icon={HiBriefcase} path="/payments" caption="Payments" isDb />
-					<div className="border-white border-b-2 w-10 mt-3 mb-4 ml-[26px]"></div>
-					<SidebarItem currentPath={currentLocation} Icon={FaCommentDots} path="/support" caption="Support" isDb />
-					<SidebarItem currentPath={currentLocation} Icon={AiFillSetting} path="/" caption="" isDb />
+					{/*<SidebarItem currentPath={currentLocation} Icon={FaCommentDots} path="/support" caption="Support" isDb /> */}
+					<SidebarItem currentPath={currentLocation} Icon={AiFillSetting} path="/" caption="Settings" isDb />
 				</ul>
 			</div>
 			{/* <div className="mt-8">
